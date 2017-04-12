5620e214478cdc314b31e1c468d61ad1c7b01408
Lion Pierau <lion.pierau@gmail.com>
2017-04-12 20:36:42 +0200

    4.) works but is not perfect

--- 4-key_value_db/src/testPersistence.py
+++ 4-key_value_db/src/testPersistence.py
@@ -4,25 +4,51 @@ import redis
 
 # https://pypi.python.org/pypi/redis
 
-db = redis.StrictRedis(host='localhost', port=6379, db=0)
-pipe = db.pipeline()
+# REDIS
+REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
+REDIS_PIPE = REDIS.pipeline()
+
+# FILE
+FILE_PATH = 'resources/plz_mini.data'
+
+# PATTERN
+KEY_PATTERN = '"([_a-zA-Z]+)"'
+NUM_PATTERN = '.\-0-9'
 
 
-def dict_from_string(str):
+def getDictionaryFromString(string):
+    """Returns a Map/Dictionary from a String with KEY VALUE pairs"""
     result = {}
-    for match in regex.findall('"([_0-9a-zA-Z]*)" : "([0-9a-zA-Z]*)"', str):
+    # Look for "<KEY>" : "<VALUE>"
+    for match in regex.findall(KEY_PATTERN + ' : "([ 0-9a-zA-Z]+)"', string):
         result[match[0]] = match[1]
+    # Look for "<KEY>" : <NUM_VALUE>
+    for match in regex.findall(KEY_PATTERN + ' : ([.\-0-9]+)', string):
+        result[match[0]] = float(match[1])
+    # Look for "<KEY>" : [<NUM_VALUES, ... >]
+    for match in regex.findall(KEY_PATTERN + ' : \[([.,\- 0-9]+)\]', string):
+        result[match[0]] = [float(i) for i in (match[1].split(','))]
+
+        print(result)
     return result
 
 
 # GET FROM FILE
 dict_from_line = {}
-with open('resources/plz_mini.data', 'r') as inf:
-    for line in inf:
-        dict_from_line = dict_from_string(line)
-        # SAVE IN REDIS
-        id = dict_from_line.pop("_id")
-        db.hmset(id, dict_from_line)
+with open(FILE_PATH, 'r') as file:
+    for line in file:
+        # Convert line to Map/Dictionary
+        dict_from_line = getDictionaryFromString(line)
 
-result = db.hgetall("01001")
+        # SAVE IN REDIS
+        identifier = dict_from_line.pop('_id')
+        state = dict_from_line.pop('state')
+        city = dict_from_line.pop('city')
+        key = identifier + ':' + state + ':' + city
+        REDIS.set(key + '#pop', dict_from_line['pop'])
+        REDIS.set(key + '#loc', dict_from_line['loc'])
+
+# TODO Get KEYS and get values
+
+result = REDIS.get('01001:MA:AGAWAM#loc')
 print(result)
